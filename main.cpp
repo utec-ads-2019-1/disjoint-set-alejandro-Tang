7329f3ba70d6279308c518cbe90ffb4d4ff91f54
alejandro <alejandro.tang@utec.edu.pe>
2019-05-06 19:57:24 -0500

    Se añadió join según size

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void joinAndPrintRank(int a, int b, vector<Node<int> *> *vector){
     (*vector)[a-1]->joinByRank((*vector)[b-1]);
     cout << "join(" << a <<", " << b << "):\n";
     for(int i = 1; i<=(*vector).size(); i++){
-        cout << "El root del nodo " << i << " es " << ((*vector)[i-1]->findRoot())->getData() << endl;
+        cout << "El root del nodo " << i << " es " << ((*vector)[i-1]->findRoot())->getData() << " y el tamaño de este es ahora " << ((*vector)[i-1]->findRoot())->getSize() << endl;
     }
     cout << endl;
 }
@@ -22,7 +22,16 @@ void joinAndPrintArbitrario(int a, int b, vector<Node<int> *> *vector){
     (*vector)[a-1]->joinArbitrary((*vector)[b-1]);
     cout << "join(" << a <<", " << b << "):\n";
     for(int i = 1; i<=(*vector).size(); i++){
-        cout << "El root del nodo " << i << " es " << ((*vector)[i-1]->findRoot())->getData() << endl;
+        cout << "El root del nodo " << i << " es " << ((*vector)[i-1]->findRoot())->getData() << " y el tamaño de este es ahora " << ((*vector)[i-1]->findRoot())->getSize() << endl;
+    }
+    cout << endl;
+}
+
+void joinAndPrintBySize(int a, int b, vector<Node<int> *> *vector){
+    (*vector)[a-1]->joinBySize((*vector)[b-1]);
+    cout << "join(" << a <<", " << b << "):\n";
+    for(int i = 1; i<=(*vector).size(); i++){
+        cout << "El root del nodo " << i << " es " << ((*vector)[i-1]->findRoot())->getData() << " y el tamaño de este es ahora " << ((*vector)[i-1]->findRoot())->getSize() << endl;
     }
     cout << endl;
 }
@@ -38,15 +47,18 @@ int main(){
 
     auto nodeVector1 = new vector<Node<int>*>;
     auto nodeVector2 = new vector<Node<int>*>;
+    auto nodeVector3 = new vector<Node<int>*>;
 
     for(int i = 1; i < 17; i++){
         auto node1 = new Node<int>(i);
         auto node2 = new Node<int>(i);
+        auto node3 = new Node<int>(i);
         nodeVector1->push_back(node1);
         nodeVector2->push_back(node2);
+        nodeVector3->push_back(node3);
     }
 
-    cout << "Se realizan los joins por rank: \n";
+    cout << "-----------------------------------------------------\nSe realizan los joins según su rank: \n";
     joinAndPrintRank(1, 2, nodeVector1);
     joinAndPrintRank(3, 4, nodeVector1);
     joinAndPrintRank(3, 5, nodeVector1);
@@ -95,7 +107,28 @@ int main(){
     }
     delete nodeVector2;
 
+    cout << "-----------------------------------------------------\nSe realizan los joins según su size: \n";
+    joinAndPrintBySize(1, 2, nodeVector3);
+    joinAndPrintBySize(3, 4, nodeVector3);
+    joinAndPrintBySize(3, 5, nodeVector3);
+    joinAndPrintBySize(1, 7, nodeVector3);
+    joinAndPrintBySize(3, 6, nodeVector3);
+    joinAndPrintBySize(8, 9, nodeVector3);
+    joinAndPrintBySize(1, 8, nodeVector3);
+    joinAndPrintBySize(3, 10, nodeVector3);
+    joinAndPrintBySize(3, 11, nodeVector3);
+    joinAndPrintBySize(3, 12, nodeVector3);
+    joinAndPrintBySize(3, 13, nodeVector3);
+    joinAndPrintBySize(14, 15, nodeVector3);
+    joinAndPrintBySize(16, 1, nodeVector3);
+    joinAndPrintBySize(14, 16, nodeVector3);
+    joinAndPrintBySize(1, 3, nodeVector3);
+    joinAndPrintBySize(1, 14, nodeVector3);
 
+    for(int i = 1; i < 17; i++){
+        delete (*nodeVector3)[i-1];
+    }
+    delete nodeVector3;
 
 
     return 0;
